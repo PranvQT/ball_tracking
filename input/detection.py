b5b06920e6da75aa89ef860d2f4459a1225566ce
QT-PC5 <pranav@quidich.com>
2022-12-27 16:22:49 +0530

    Adding detection code

--- input/detection.py
+++ input/detection.py
@@ -1,15 +1,42 @@
-import json
-
-import numpy as np
-import torch
-from models.experimental import attempt_load
-from Utils.datasets import letterbox
-from Utils.general import check_img_size, non_max_suppression, scale_coords
-from Utils.torch_utils import select_device
-
 
 # complete code has to be written based on the model
+
+def detect(image1,image2,image3):
+    x1 = image1[...,::-1]
+    x2 = image2[...,::-1]
+    x3 = image3[...,::-1]
+    #Convert np arrays to PIL images
+    x1 = array_to_img(x1)
+    x2 = array_to_img(x2)
+    x3 = array_to_img(x3)
+    #Resize the images
+    x1 = x1.resize(size = (WIDTH, HEIGHT))
+    x2 = x2.resize(size = (WIDTH, HEIGHT))
+    x3 = x3.resize(size = (WIDTH, HEIGHT))
+    #Convert images to np arrays and adjust to channels first
+    x1 = np.moveaxis(img_to_array(x1), -1, 0)		
+    x2 = np.moveaxis(img_to_array(x2), -1, 0)		
+    x3 = np.moveaxis(img_to_array(x3), -1, 0)
+    #Create data
+    unit.append(x1[0])
+    unit.append(x1[1])
+    unit.append(x1[2])
+    unit.append(x2[0])
+    unit.append(x2[1])
+    unit.append(x2[2])
+    unit.append(x3[0])
+    unit.append(x3[1])
+    unit.append(x3[2])
+    unit=np.asarray(unit)	
+    unit = unit.reshape((1, 9, HEIGHT, WIDTH))
+    unit = unit.astype('float32')
+    unit /= 255
+    y_pred = model.predict(unit, batch_size=BATCH_SIZE)
+
+    return y_pred
+
 '''
+
 imgsz = 1120
 conf_thres = 0.66
 device = ""
